b7b2ab0b145c6df16cbb5651e998b0333cee5bb8
TOMJID <tomjidhuda04@gmail.com>
2026-01-02 20:42:53 +0600

    feat: configure CORS with frontend origin and credentials
    - Install CORS type definitions
    - Enable CORS middleware in the application setup
    - Configure allowed origin and credentials for secure client-side requests

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app: Application = express();
 
 //? json Parser
 app.use(express.json());
+
+//! CROS setup
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 //! better auth
 app.all("/api/auth/{*any}", toNodeHandler(auth));
