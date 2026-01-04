c5f06311536f0b412ffaedf377511d4386ddca70
TOMJID <tomjidhuda04@gmail.com>
2026-01-04 22:25:06 +0600

    feat: add nodemailer for email verification and update dependencies
    - Added nodemailer to handle email sending functionality.
    - Implemented email verification logic in auth module using nodemailer.
    - Updated package.json to include @types/nodemailer and nodemailer dependencies.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,19 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+// Create a transporter using Ethereal test credentials.
+// For production, replace with your actual SMTP server details.
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.GMAIL_NAME,
+    pass: process.env.GMAIL_APP_PASS,
+  },
+});
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -36,9 +49,17 @@ export const auth = betterAuth({
   //?
   emailVerification: {
     sendVerificationEmail: async ({ user, url, token }, request) => {
-      console.log(
-        `Send verification email to ${user.email} \n with url: ${url} \n and token: ${token}`
-      );
+      const verificationToken = `${process.env
+        .FRONTEND_URL!}/verify-email?token=${token}`;
+      const info = await transporter.sendMail({
+        from: '"prisma blog" <tomjidhuda04@gmail.com>',
+        to: "hudatomjid@gmail.com",
+        subject: "hello form blog site",
+        text: "hi",
+        html: "<b>hello world!</b>",
+      });
+
+      console.log("message send:", info.messageId);
     },
   },
 });
