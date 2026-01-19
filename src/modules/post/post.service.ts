2bdf91b9eb4eeaf274d897ab91e15be6174a727d
TOMJID <tomjidhuda04@gmail.com>
2026-01-19 21:46:22 +0600

    style: Add trailing comma to `createPost` function parameter.

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -2,7 +2,7 @@ import { Post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async (
-  data: Omit<Post, "id" | "createdAt" | "updatedAt">
+  data: Omit<Post, "id" | "createdAt" | "updatedAt">,
 ) => {
   const result = await prisma.post.create({
     data,
