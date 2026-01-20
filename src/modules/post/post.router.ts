fea554aba1aeff822b0504266273ed0a2cc79e5b
TOMJID <tomjidhuda04@gmail.com>
2026-01-20 15:44:12 +0600

    feat(post): add session check middleware to create post route
    - Introduce inline `auth` middleware in `post.router.ts` to retrieve user session via betterAuth.
    - Apply auth middleware to the create post endpoint to log session details.

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,8 +1,19 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { PostController } from "./post.controller";
-
 const router = express.Router();
+import { auth as betterAuth } from "../../lib/auth";
 
-router.post("/", PostController.CreatePost);
+const auth = (...roles: any) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    //? getting user session
+    const session = await betterAuth.api.getSession({
+      headers: req.headers as any,
+    });
+    console.log(session);
+    next();
+  };
+};
+
+router.post("/", auth("ADMIN", "USER"), PostController.CreatePost);
 
 export const postRouter = router;
