6af43bff7c2b56b383fe47d9e88c85b6be73d65e
TOMJID <tomjidhuda04@gmail.com>
2026-01-22 19:05:31 +0600

    feat: improve admin seeding script with auto-verification and logging
    - Add detailed console logs to track seeding progress.
    - Implement automatic email verification for newly created admin users.
    - Update admin email configuration for initial setup.

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -3,14 +3,16 @@ import { UserRole } from "../middlewares/auth";
 
 async function seedAdmin() {
   try {
+    console.log("***** Admin seeding started *****");
     const adminData = {
       name: "Admin tomjid",
-      email: "tomjidhuda05@gmail.com",
+      email: "tomjidhuda06@gmail.com",
       role: UserRole.ADMIN,
       password: "pass12345",
     };
 
     //? check if the user exists on db or not
+    console.log("***** Checking if the user exists or not *****");
     const existingUser = await prisma.user.findUnique({
       where: {
         email: adminData.email,
@@ -21,7 +23,7 @@ async function seedAdmin() {
       throw new Error("User already exists!");
     }
 
-    console.log("Creating admin via API...");
+    console.log("***** Creating admin via API... *****");
 
     const createAdmin = await fetch(
       "http://localhost:3000/api/auth/sign-up/email",
@@ -33,7 +35,21 @@ async function seedAdmin() {
         body: JSON.stringify(adminData),
       },
     );
-    console.log(createAdmin);
+
+    //? if the create has been created successfully the we update the emailVerified to true
+    if (createAdmin.ok) {
+      await prisma.user.update({
+        where: {
+          email: adminData.email,
+        },
+        data: {
+          emailVerified: true,
+        },
+      });
+      console.log("***** Admin emailVerified successful *****");
+    }
+
+    console.log("***** Admin created successfully *****");
   } catch (error) {
     console.log(error);
     process.exit(1);
