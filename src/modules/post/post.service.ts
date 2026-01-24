06fe88c6c9cd538347b73f8ad22001f642b4b6e3
TOMJID <tomjidhuda04@gmail.com>
2026-01-24 11:54:42 +0600

    feat(post): add nested comment retrieval to post details

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,8 @@
-import { Post, PostStatus } from "../../../generated/prisma/client";
+import {
+  CommentStatus,
+  Post,
+  PostStatus,
+} from "../../../generated/prisma/client";
 import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
@@ -117,6 +121,28 @@ const getPostById = async (postId: string) => {
     const postData = await timeline.post.findUnique({
       where: {
         id: postId,
+      },
+      include: {
+        comments: {
+          where: {
+            parentId: null,
+            status: CommentStatus.APPROVED,
+          },
+          include: {
+            replies: {
+              where: {
+                status: CommentStatus.APPROVED,
+              },
+              include: {
+                replies: {
+                  where: {
+                    status: CommentStatus.APPROVED,
+                  },
+                },
+              },
+            },
+          },
+        },
       },
     });
     return postData;
